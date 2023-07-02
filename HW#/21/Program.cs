6f7377229a124ea96957721c233c9837dd76322a
Павел <pashastoliarov442@gmail.com>
2023-07-02 16:24:55 +0300

    feat: homework

---
+++ HW#/21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Distance (double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+}
+
+Console.WriteLine("Enter numbers: ");
+
+double num1 = double.Parse(Console.ReadLine()!);
+double num2 = double.Parse(Console.ReadLine()!);
+double num3 = double.Parse(Console.ReadLine()!);
+double num4 = double.Parse(Console.ReadLine()!);
+double num5 = double.Parse(Console.ReadLine()!);
+double num6 = double.Parse(Console.ReadLine()!);
+
+double result = Distance(num1, num2, num3, num4, num5, num6);
+Console.WriteLine(result);
---
+++ HW#/23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void power (int N)
+{
+    int help = 1;
+    if(N > 1)
+    {
+        while(help <= N)
+        {
+            Console.Write($"{Math.Pow(help, 3)} ");
+            help++;
+        }
+    }
+    else
+    {
+        while(help >= N)
+        {
+            Console.Write($"{Math.Pow(help, 3)} ");
+            help--;
+        }
+    }
+}
+
+
+int num = int.Parse(Console.ReadLine()!);
+power(num);
