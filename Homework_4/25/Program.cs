34df2c2644aab3816a475c4261f9ccad52ce5908
Павел <pashastoliarov442@gmail.com>
2023-07-06 16:20:25 +0300

    feat: homework

---
+++ Homework_4/25/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+int Power (int Base, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= Base;        
+    }
+    return power;
+}
+
+Console.Write("Enter base and positive exponent: ");
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+
+int result = Power(A, B);
+Console.WriteLine(result);
+
+
+void negative_exponent (double Base, double Negative_exponent)
+{
+    double negative_power = 1;
+    for (int i = 0; i > Negative_exponent; i--)
+    {
+        negative_power /= Base; 
+    }
+    Console.WriteLine(negative_power);
+}
+
+Console.Write("Enter Base and negative exponent: ");
+double A2 = double.Parse(Console.ReadLine()!);
+double B2 = double.Parse(Console.ReadLine()!);
+
+negative_exponent(A2, B2);
---
+++ Homework_4/29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray (int[] EmptyArray)
+{
+    int length = EmptyArray.Length;
+    for (int i = 0; i < length; i++)
+    {
+        EmptyArray[i] = new Random().Next(-10, 40);
+    }
+}
+
+void PrintArray (int[] FilledArray)
+{
+    int size = FilledArray.Length;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(FilledArray[i] + " ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
