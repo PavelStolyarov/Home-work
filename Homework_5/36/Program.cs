b713986e35720d56f5d0bdd194aa94291470a77b
Павел <pashastoliarov442@gmail.com>
2023-07-09 14:38:09 +0300

    feat: homewor 5

---
+++ Homework_5/36/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void FillArray(int[] array, int ArraySize, int start, int end)
+{
+    start = 1;
+    end = 1000;
+    for (int i = 0; i < ArraySize; i++)
+    {
+        array[i] = new Random().Next(start, end + 1);
+    }
+
+}
+
+void PrintArray(int[] massive, int MassiveSize)
+{
+    for (int i = 0; i < MassiveSize; i++)
+    {
+        Console.Write($"{massive[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void OddSum (int[] Massive, int length )
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if(i % 2 != 0) sum += Massive[i];
+    }
+    Console.WriteLine($"{sum}");
+}
+
+Console.WriteLine("Enter size, and bounds");
+int NumberOfElements = int.Parse(Console.ReadLine()!);
+int MinValue = int.Parse(Console.ReadLine()!);
+int MaxValue = int.Parse(Console.ReadLine()!);
+int[] arr = new int[NumberOfElements];
+
+FillArray(arr, NumberOfElements, MinValue, MaxValue);
+PrintArray(arr, NumberOfElements);
+OddSum(arr, NumberOfElements);
