eaf841b780c8317052338d079119aff0be20de0c
Павел <pashastoliarov442@gmail.com>
2023-07-23 22:05:29 +0300

    feat: homework9

---
+++ Homework_9/1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+ 
+void Nums(int number, int num)
+{
+    if (num - 1 == number)
+    {
+        return;
+    }
+    if(number % 2 == 0) 
+    {
+        Nums(number - 1, num);
+        Console.Write(number + " ");
+    }
+    else 
+    {
+        Nums(number - 1, num);
+    }
+}
+int M = int.Parse(Console.ReadLine()!);
+int N = int.Parse(Console.ReadLine()!);
+Nums(N, M);
